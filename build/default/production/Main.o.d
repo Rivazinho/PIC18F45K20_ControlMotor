64190c4d26cb07d7abe8124f76c48d6eb66d37ff
Rivazinho <darkryvaz@gmail.com>
2022-06-10 22:27:07 +0200

    PIC18F45K20_ControlMotor

---
+++ build/default/production/Main.o.d
@@ -0,0 +1,3 @@
+build/default/production/Main.o:  \
+	D:/Bruno/Documents/4º\ -\ 2º\ Cuatrimestre/Laboratorio\ de\ sistemas\ digitales\ programables/Proyecto1_Control_Motor_DC/Proyecto_1_Control_Motor/Proyecto_1.X/Main.asm  \
+
